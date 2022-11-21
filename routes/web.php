7539b97a2e6f798d5fc735af9d9f104285d4f948
AsZaychik <aszaychik@gmail.com>
2022-11-21 09:51:30 +0700

    Controller Test

--- routes/web.php
+++ routes/web.php
@@ -83,3 +83,4 @@ Route::get("/produk-redirect/{id}", function ($id)
     return redirect()->route("product.detail", ["id" => $id]);
 });
 
+Route::get("/controller/hello/{name}", [\App\Http\Controllers\HelloController::class, 'hello']);
